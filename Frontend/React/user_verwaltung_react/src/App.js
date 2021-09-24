0094f2018a6a6984c21413cf71d5224441d0f414
Matthias W <matthias@svax.io>
2021-09-24 20:09:45 +0200

    Add basic rest functionality for react

--- Frontend/React/user_verwaltung_react/src/App.js
+++ Frontend/React/user_verwaltung_react/src/App.js
@@ -1,35 +1,59 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import userDetail from './components/UserDetail';
 import Loading from './components/Loading';
 
+let userId = 1;
+
+
 function App() {
+  const User = GetUser();
+
+  return (
+    <div className='App'>
+      <div className='container'>
+        <h1>Loaded User</h1>
+      </div>
+      <User />
+    </div>
+  );
+}
+
+const GetUser = () => {
   const UserLoading = Loading(userDetail);
+
   const [appState, setAppState] = useState({
     loading: false,
     user: null,
   });
 
-  useEffect(() => {
+  const handleClick = (e) => {
     setAppState({ loading: true });
-    const userId = 1;
+
+    userId++;
+
+    if (userId > 3) {
+      userId = 1;
+    }
+
     const apiUrl = 'https://localhost:5001/Users/' + userId;
     fetch(apiUrl)
       .then((res) => res.json())
       .then((user) => {
         setAppState({ loading: false, user: user });
       });
-  }, [setAppState]);
-  return (
-    <div className='App'>
-      <div className='container'>
-        <h1>Loaded User</h1>
-      </div>
-      <div className='user-container'>
-        <UserLoading isLoading={appState.loading} user={appState.user} />
-      </div>
-    </div>
-  );
+  }
+
+  return class extends React.Component {
+    render() {
+      return (
+        <div className='user-container'>
+          <UserLoading isLoading={appState.loading} user={appState.user} />
+          <button variant="contained" onClick={handleClick}>Get Random User</button>
+        </div>
+      );
+    }
+  };
 }
 
 export default App;
